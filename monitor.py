fa684e2e06705c623d26f4b637d0a1ce4990394b
Kaushal Anbarasan <kaushal.anba@gmail.com>
2024-06-01 22:20:34 -0700

    updated webhook to include price difference

--- monitor.py
+++ monitor.py
@@ -80,14 +80,14 @@ class Monitor:
                             floor = tickets[0]["price"]["selling"]["value"]
                             currency = tickets[0]["price"]["selling"]["currency"]
                             if floor < listing[4]:
-                                listing[4] = floor
 <<<<<<< Updated upstream
                                 print(f"New price!! {floor=}")
                                 #self.send_webhook(listing[3], floor)
 =======
                                 self.settings_manager.set_listings(self.listings)
                                 print(f"New price!! {floor=}")
-                                # self.webhook_manager.send_webhook(listing[3], floor)
+                                self.webhook_manager.send_webhook(listing[3], floor, listing[4])
+                                listing[4] = floor
 >>>>>>> Stashed changes
                             else:
                                 print("no change")
--- webhook_manager.py
+++ webhook_manager.py
@@ -11,11 +11,25 @@ class WebhookManager():
     def set_webhook_url(self, webhook_url):
         self.webhook_url = webhook_url
         
-    def send_webhook(self, nickname, new_price):
+    def send_webhook(self, nickname, new_price, old_price):
         if self.webhook == "":
             print("Set webhook!")
             return
-        message = {"username": nickname, "content": new_price}
+        percent_change = round(abs(old_price - new_price) / old_price * 100, 1)
+        change = "increase" if new_price >= old_price else "decrease"
+        message = {
+          "content": null,
+          "embeds": [
+            {
+              "title": f"{nickname}",
+              "description": f"The new lowest price for event is: {floor}\nThis is a {percent_change} {change}",
+              "url": "https://www.google.com",
+              "color": 11478235
+            }
+          ],
+          "username": "ticombo monitor",
+          "attachments": []
+        } 
         r = requests.post(self.webhook, data=message)
         if 200 <= r.status_code < 300:
             print(f"Webhook sent {result.status_code}")
